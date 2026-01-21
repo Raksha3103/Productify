3cd0733d01e1b6e030371915038a4be5444d6304
raksha <raksha.achary@winwire.com>
2026-01-21 11:34:35 +0530

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import {ENV} from "./config/env"
+import cors from "cors"
+import { clerkMiddleware } from '@clerk/express'
+
+const app=express();
+app.use(cors({origin:ENV.FRONTEND_URL}));
+app.use(clerkMiddleware());//auth object will be attavhed to the req
+app.use(express.json());//parses json request bodies
+app.use(express.urlencoded({extended:true}));//parses urlencoded request bodies
+
+app.get("/",(req,res)=>{
+
+    res.json({
+        message:"PostgreSQL with Express and TypeScript",
+        endpoints:{
+            users:"api/users",
+            products:"api/products",
+            comments:"api/comments",
+
+        }
+    })
+})
+app.listen(ENV.PORT,()=>console.log("Server is up and running on port:",ENV.PORT))
